f5b73f1757f3b2b6b03e1f7a3c5e3717ced1201f
Simon Oulevay <simon.oulevay@gmail.com>
2018-06-27 13:51:22 +0200

    Validate metrics

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,56 @@
-const { has, isArray, uniq } = require('lodash');
+const { has, isArray, isFinite, isInteger, isPlainObject, uniq, values } = require('lodash');
+const moment = require('moment');
 
-exports.METRIC_TYPES = {
-  boolean: 'true or false',
-  bytes: 'an integer greater than or equal to zero',
-  datetime: 'an date in ISO-8601 format',
-  number: 'a numerical value with no associated unit of measurement',
-  quantity: 'an integer greater than or equal to zero representing an amount of something',
-  seconds: 'an integer greater than or equal to zero'
+const METRIC_TYPES = {
+  boolean: {
+    description: 'true or false',
+    validate: v => typeof v === 'boolean'
+  },
+  bytes: {
+    description: 'an integer greater than or equal to zero',
+    validate: v => isInteger(v) && v >= 0
+  },
+  datetime: {
+    description: 'a date in ISO-8601 format',
+    validate: v => moment(v).isValid()
+  },
+  number: {
+    description: 'a number with no associated unit of measurement',
+    validate: isFinite
+  },
+  quantity: {
+    description: 'an integer greater than or equal to zero representing an amount of something',
+    validate: v => isInteger(v) && v >= 0
+  },
+  seconds: {
+    description: 'a number greater than or equal to zero',
+    validate: v => isFinite(v) && v >= 0
+  }
 };
 
 exports.buildMetric = function(name, type, value, description, tags = {}) {
+  if (typeof name !== 'string') {
+    throw new Error(`Metric name must be a string, got ${typeof name}`);
+  } else if (!name.match(/^[a-z0-9]+(?:[A-Z0-9][a-z0-9]+)*$/)) {
+    throw new Error(`Metric name "${name}" is not in camel-case`);
+  } else if (!METRIC_TYPES[type]) {
+    throw new Error(`Unknown metric type "${type}"; must be one of the following: ${Object.keys(METRIC_TYPES).map(t => `"${t}"`).join(', ')}`);
+  } else if (value !== null && !METRIC_TYPES[type].validate(value)) {
+    throw new Error(`Invalid metric value ${JSON.stringify(value)} for type "${type}": must be ${METRIC_TYPES[type].description}`);
+  } else if (typeof description !== 'string') {
+    throw new Error(`Metric description must be a string, got ${typeof description}`);
+  } else if (description.match(/^\s*$/)) {
+    throw new Error('Metric description cannot be blank');
+  } else if (!isPlainObject(tags)) {
+    throw new Error(`Metric tags must be a plain object, got ${typeof tags}`);
+  } else if (Object.keys(tags).some(key => !key.match(/^[a-zA-Z_][a-zA-Z0-9_]*$/))) {
+    throw new Error(`Metric tag keys must contain only letters, digits and underscores, and cannot start with a digit (${Object.keys(tags).sort().join(', ')})`);
+  } else if (values(tags).some(tag => typeof tag !== 'boolean' && typeof tag !== 'number' && typeof tag !== 'string')) {
+    throw new Error(`Metric tag values must be booleans, numbers or string (${JSON.stringify(tags)})`);
+  } else if (values(tags).some(tag => String(tag).match(/"/))) {
+    throw new Error(`Metric tag values must not contain double quotes (${JSON.stringify(tags)})`);
+  }
+
   return {
     description,
     name,
